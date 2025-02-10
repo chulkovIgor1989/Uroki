54924cf9295c26a53c60e496e9d050e96debdba8
Igor <chulkov.2025@inbox.ru>
2025-02-11 00:03:11 +0500

    Revert "wer"
    This reverts commit 88aa494de83bb744c5ccafa22dd653e4fcc41d67.

--- org/example/Main.java
+++ org/example/Main.java
@@ -1,61 +1,23 @@
 package org.example;
-
-public class Main{
-    public static void main(String[]args){
-
-        Dog dogSharik=new Dog("Шарик");
-        Cat catFenay=new Cat("Феня");
-
-        int minValue = 1;
-        int maxValue = 240;
-        int randomValue=minValue+(int)(Math.random()*(maxValue-minValue+1));
-
-        dogSharik.run(randomValue);
-        dogSharik.swim(8);
+import org.example.Sotrudnik;
+public class Main {
+    public static void main(String[] args) {
 
 
 
+        Sotrudnik [] persArray = new Sotrudnik[5];
 
-        catFenay.run(randomValue);
-        catFenay.swim(randomValue);
+        persArray[0] = new Sotrudnik("Ivanov Ivan Fedorovich", "Engineer", "iviivan@mail.com", "8923452", 30000, 35);
+        persArray[1] = new Sotrudnik("Petr Sergeev Matveev", "QAEngineer", "dghk@mail.com", "89435436474", 40000, 33);
+        persArray[2] = new Sotrudnik("Chulkov Bogdan Igorevich", "1C", "hjsdba@mail.com", "8927630", 35000, 34);
+        persArray[3] = new Sotrudnik("Luchiy Dog Namevich", "DevOps", "ghlagh@mailbox.com", "8964527854", 32000, 34);
+        persArray[4] = new Sotrudnik("Prosto Maria Vadimovna", "Progger", "jhuihio@mailbox.com", "893455543532", 38000, 19);
 
-        System.out.println("Количество  животных: "+Animals.count);
-        System.out.println("Количество  кошек: "+ Cat.getCatCount());
-        System.out.println("Количество  собак: "+ Dog.getDogCount());
-
-
-
-        Cat[]cats=new Cat[5];
-        for(int i=0;i<cats.length;i++){
-        cats[i]=new Cat("Кот номер "+(i+1));
+        for (Sotrudnik employee : persArray) {
+            employee.SotrudnikDemon();
+            System.out.println("\n");
         }
-        Bow bow=new Bow(45);
-        for(int i=0;i<cats.length;i++){
-            int foodTaken=bow.getFood(10);
-            cats[i].eat(foodTaken);
-            if(i<cats.length-1){
-                System.out.print("\nОсталось в миске "+bow.FoodAmount());
-            }
-        }
-
-        for(Cat cat:cats){
-            if(cat.fullness){
-                System.out.println(cat.name+" сытый.");
-            }else{
-            System.out.println(cat.name+" голодный.");
-            }
-
-        }
-
-        catFenay.eat(bow.getFood(11));
-        System.out.println("Осталось в миске "+bow.FoodAmount() + " --> ");
-        bow.addFood(11);
-        catFenay.eat(bow.getFood(9));
-
     }
 }
-
-
-
 
 
